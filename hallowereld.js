ef218f387c8938e3982acd8aa97a1f7077ad8f64
hasan07mvk <hasan72.irq@yahoo.com>
2023-11-10 12:58:31 +0100

    ok

---
+++ hallowereld.js
@@ -0,0 +1,5 @@
+document.write ("hallowereld")
+alert("hallowereld");
+//var name= prompt ("wat ia jou naam")
+
+document.write ("<br><hr>toverfluitstraart 19-21 1080 sint-jans-molenbeek")
---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+$title="mijn tweede website";z
+$naam"hasan";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?php echo $title; ?</title>
+</head>
+<body>
+    
+<h1>?php echo $title; ?</h1>
+<p>Created by: <?php echo $naam;?</p>
+</body>
+</html>
